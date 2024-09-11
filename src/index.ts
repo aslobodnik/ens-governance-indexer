c4cb6c2828705d1d930721075212562058358b45
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-09-10 23:14:30 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { ponder } from '@/generated'
 
 ponder.on('ExampleContract:VoteCast', async ({ event, context }) => {
   const { Votes } = context.db
+
   await Votes.create({
     id: event.transaction.hash,
     data: event.args,
